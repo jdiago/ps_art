85c1dd912c0a8146f8bb49c8b354f14539b7637b
Josef Diago <josef.diago@gmail.com>
2012-09-14 22:25:53 -0400

    Project start! GO!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsArt::Application.config.secret_token = '7138a2beb711a91e9d479e284241b4d8571908bc077d1b51ceb1c8584d96f52687072a802a54698ef8831ddb8747a0e64afde06e5e5191e664f07a697d0aa054'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PsArt::Application.config.session_store :cookie_store, key: '_ps_art_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PsArt::Application.config.session_store :active_record_store
