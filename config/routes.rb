225712537c29cae02395ab00446fc96b37508229
Josef Diago <josef.diago@gmail.com>
2012-09-15 20:36:27 -0400

    user auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 PsArt::Application.routes.draw do
+  resources :users, except: :show
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   root to: 'home#index'
 end
