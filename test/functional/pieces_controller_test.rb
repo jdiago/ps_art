2358e49a07d71d7bd28cc649b7b7d050d5d2a6d1
Josef Diago <josef.diago@gmail.com>
2012-09-16 00:25:33 -0400

    pieces controller tests

--- test/functional/pieces_controller_test.rb
+++ test/functional/pieces_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PiecesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:markers)
+  end
+
+  test "should show piece" do
+    get :show
+    assert_response :success
+    assert_not_nil assigns(:piece)
+  end
 end
