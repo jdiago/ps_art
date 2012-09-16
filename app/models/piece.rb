3594b86ff4397b9843561eae3cde3bc84fc2198b
Josef Diago <josef.diago@gmail.com>
2012-09-16 12:46:34 -0400

    maps, finally

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,8 +1,15 @@
 class Piece < ActiveRecord::Base
-  attr_accessible :name, :latitude, :longitude
+  attr_accessible :name, :lat, :lng, :gmaps, :address
 
-  acts_as_gmappable
+  acts_as_gmappable :lat => 'lat', :lng => 'lng', :process_geocoding => :geocode?,
+                  :address => "address", :normalized_address => "address",
+                  :msg => "Sorry, not even Google could figure out where that is"
+
+  def geocode?
+    (!address.blank? && (lat.blank? || lng.blank?)) || address_changed?
+  end
 
   def gmaps4rails_infowindow
+    "<h3>#{self.name}</h3><br><a href=\"/pieces/#{self.id}\">Click for details</a>"
   end
 end
